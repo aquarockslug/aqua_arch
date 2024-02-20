9163dbf6670668a1c956b375d70a33936232e420
aquarock <aqua.rock.slug@gmail.com>
2024-02-20 16:10:06 -0600

    link zsh theme

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,7 @@
+AQUA_ARCH="$HOME/aqua_arch"
+mv zshrc ~/.zshrc
+
+ln -s $AQUA_ARCH/dracula/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme 
+
 yay -S --noconfirm - < package_list.txt
 go install github.com/maaslalani/nap@main
-git clone https://github.com/PlankCipher/kabmat.git
-cd kabmat
-make
-sudo make install
