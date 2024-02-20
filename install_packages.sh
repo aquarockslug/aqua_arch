eeec0bfec3c50810c6b46dca0c3234100c2dd95a
aquarock <aqua.rock.slug@gmail.com>
2024-02-20 16:17:06 -0600

    link nvim

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,8 @@
 AQUA_ARCH="$HOME/aqua_arch"
-mv zshrc ~/.zshrc
-
+sudo pacman -Syu
+sudo pacman -S --noconfirm - < package_list.txt
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 ln -s $AQUA_ARCH/dracula/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme 
-
-yay -S --noconfirm - < package_list.txt
+ln -s $AQUA_ARCH/aqua_nvim ~/.config/nvim
+mv zshrc ~/.zshrc
 go install github.com/maaslalani/nap@main
