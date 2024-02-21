2fa1f6454c7a4b3ea9fea33c8cfaf87bb8226c51
aquarock <aqua.rock.slug@gmail.com>
2024-02-21 05:52:46 -0600

    nvim PlugInstall

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 AQUA_ARCH="$HOME/aqua_arch"
+
 sudo pacman -Syu
 sudo pacman -S --noconfirm - < package_list.txt
+
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
 ln -s $AQUA_ARCH/dracula/zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme 
 ln -s $AQUA_ARCH/aqua_nvim $HOME/.config/nvim
 ln -s $AQUA_ARCH/zshrc $HOME/.zshrc
+
+nvim -c PlugInstall
